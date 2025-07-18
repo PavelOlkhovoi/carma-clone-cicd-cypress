9df027c500a2e5a02a788b1f760569772c8c208c
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-18 17:45:09 +0200

    Revert last two commits: 'add new settings report' and 'set reporter in geoportal'

--- e2e/topicmaps/stadtplan/cypress.config.ts
+++ e2e/topicmaps/stadtplan/cypress.config.ts
@@ -5,26 +5,12 @@ export default defineConfig({
   e2e: {
     ...nxE2EPreset(__dirname),
     screenshotsFolder: './report-cy/screenshots',
-    reporter: 'spec',
-    reporterOptions: {
-      toConsole: true,
-      output: 'minimal'
-    },
-    video: false,
-    videosFolder: './report-cy/videos',
-    screenshotOnRunFailure: true,
     pageLoadTimeout: 120000,
     baseUrl: 'http://localhost:4200',
     experimentalStudio: true,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.name === 'chrome' || browser.name === 'electron') {
-          launchOptions.args = launchOptions.args || [];
-          launchOptions.args.push('--silent');
-        }
-        return launchOptions;
-      });
-    }
+      // implement node event listeners here
+    },
   },
 });
