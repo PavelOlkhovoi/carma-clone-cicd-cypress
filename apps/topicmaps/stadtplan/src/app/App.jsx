c367bde323db7ba92ee21219bd6908453cf22d2e
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-07-22 14:23:23 +0200

    force deploy

--- apps/topicmaps/stadtplan/src/app/App.jsx
+++ apps/topicmaps/stadtplan/src/app/App.jsx
@@ -27,7 +27,6 @@ if (typeof global === "undefined") {
 function App() {
   const [poiColors, setPoiColors] = useState();
   const { progress, showProgress, handleProgressUpdate } = useProgress();
-  console.log('xxx ', progress)
   useEffect(() => {
     getPOIColors(setPoiColors);
     document.title = "Online-Stadtplan Wuppertal";
