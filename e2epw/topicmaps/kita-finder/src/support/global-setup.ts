42ac2a1aa2d2249fac7e5f088c50ca39e87ea16f
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-18 11:06:56 +0200

    remove unused files

--- e2epw/topicmaps/kita-finder/src/support/global-setup.ts
+++
@@ -1,9 +0,0 @@
-import { chromium, FullConfig } from '@playwright/test';
-
-async function globalSetup(config: FullConfig) {
-  // Global setup logic can be added here
-  // For example: database seeding, authentication setup, etc.
-  console.log('Running global setup for Playwright tests...');
-}
-
-export default globalSetup;
